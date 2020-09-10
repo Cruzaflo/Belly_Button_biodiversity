0a8faf895d30c11b2ae406efa03ace63818e2f15
Cruzaflo <64674430+Cruzaflo@users.noreply.github.com>
2020-09-10 12:32:14 -0700

    app complete

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,5 @@
-var dropDown = d3.select("#selDataset");
+const dropDown = d3.select("#selDataset");
+const metaPanel = d3.select("#sample-metadata")
 
 // populate drop down menu 
 d3.json("samples.json").then(function(data){
@@ -9,25 +10,23 @@ d3.json("samples.json").then(function(data){
     optionChanged(dataNames[0])
 })
 
-// horizontal bar chart
-
-function optionChanged(name) {
-    //Bubble Chart Variables
-    var otuIds = []
-    var otuSamples = []
-    var labels = []
-
-    //Bar Chartt Varibales
-    var yticks = []
-    var xValues = []
-    
-    //get otu Ids and Samples
-    
+function optionChanged(name) {    
     d3.json("samples.json").then(function(data){
+
+        //Populate meta daata panel
+        metaData(name)
+
         var samples = data['samples']
 
+        //Bubble Chart Variables
+        var otuIds = []
+        var otuSamples = []
+        var labels = []
+
+        //Bar Chart Varibales
+        var yticks = []
+        var xValues = []
         //for each dictionary in the samples array
-        //push values to chart variables
         samples.forEach(function(object){
             //Check to see if 
             //selected subject ID numbers is found in object
@@ -46,8 +45,6 @@ function optionChanged(name) {
                         yticks.push(`OTU ${object['otu_ids'][i]}`)
                         xValues.push(object['sample_values'][i])
                       }
-                    // otuIds.push(object['otu_ids'])
-                    // otuSamples.push(object['sample_values'])
                 }
                 //If the length is less than 10
                 //push the entire array to the Bar chart values
@@ -57,85 +54,52 @@ function optionChanged(name) {
                     xValues.push(object['sample_values'][i]);
                     }
                 }
+            }
+        })
+        
+        //bar plot
+        var dataSet = [{
+            type: 'bar',
+            x: xValues.reverse(),
+            y: yticks.reverse(),
+            orientation: 'h'
+        }]
+        Plotly.newPlot('bar', dataSet)
 
+        //bubble chart
+        var bubbleLayout = {
+            title: "Bacteria Culters Per Sample",
+            hovermode: "closest",
+            xaxis: { title: "OTU ID"},
+            margin: {t: 30}
+        }
+
+        var bubbleData = [
+            {
+                x: otuIds,
+                y: otuSamples,
+                text: labels,
+                mode: "markers",
+                marker: {
+                    size: otuSamples,
+                    color: otuIds,
+                    colorscale: "Earth"
+                }
+            }
+        ]
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout);  
+    })
+}
+
+function metaData(input){
+    var node = document.getElementById('sample-metadata');
+    node.innerHTML = "";
+    d3.json("samples.json").then(function(data) {
+        var metadata = data.metadata
+        metadata.forEach(function(object){
+            if (object.id == input) {
+                Object.entries(object).forEach(([key, value]) => metaPanel.append("h6").text(`${key.toUpperCase()}: ${value}`))
             }
         })
     })
-    //bar plot
-    var dataSet = [{
-        type: 'bar',
-        x: xValues.reverse(),
-        y: yticks.reverse(),
-        orientation: 'h'
-    }]
-    Plotly.newPlot('bar', dataSet)
-
-    //bubble chart
-    
-    var bubbleLayout = {
-        title: "Bacteria Culters Per Sample",
-        hovermode: "closest",
-        xaxis: { title: "OTU ID"},
-        margin: {t: 30}
-    }
-
-    var bubbleData = [
-        {
-            x: otuIds,
-            y: otuSamples,
-            text: labels,
-            mode: "markers",
-            marker: {
-                size: otuSamples,
-                color: otuIds,
-                colorscale: "Earth"
-            }
-        }
-    ]
-    Plotly.newPlot("bubble", bubbleData, bubbleLayout);  
-    console.log(otuIds)
-    console.log(otuSamples)
-    console.log(labels)
-    console.log(yticks)
-    console.log(xValues)
-    console.log(dataSet)
-    console.log(bubbleData)
-
 }
-
-
-// var dataSet = [{
-//     type: 'bar',
-//     x: otuSamples,
-//     y: otuIds,
-//     orientation: 'h'
-// }]
-
-// Plotly.newPlot('bar', dataSet)
-
-
-
-// You can also define an anonymous function inline
-// students.forEach(function(name) {
-//     console.log(name);
-//   });
-
-// var dataSet = [{
-//     type: 'bar',
-//     x: [20, 14, 23],
-//     y: ['giraffes', 'orangutans', 'monkeys'],
-//     orientation: 'h'
-//   }];
-  
-//   Plotly.newPlot('bar', data);
-
-
-
-// // Use `Object.values` and `forEach` to iterate through keys
-// Object.keys(userInfo).forEach(key => console.log(key));
-
-// // Use `Object.values` and `forEach` to iterate through values
-// Object.values(userInfo).forEach(value => console.log(value));
-
-// // Use `Object.entries` and `forEach` to iterate through keys and values
-// Object.entries(userInfo).forEach(([key, value]) => console.log(`Key: ${key} and Value ${value}`));
